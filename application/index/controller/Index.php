297bfff795299aaedb053294956798fc172481bd
mfkdgefhn <mfkdgefhn@163.com>
2019-01-03 16:44:41 +0800

    enter

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -8,6 +8,9 @@ class Index{
     public function index(){
         return 'index';
     }
+    public function index1($name='wordl'){
+        return 'index '.$name;
+    }
     public function hello($name='world',$city=''){
         return 'hello,'.$name.'! You come from '.$city.'.';
     }
